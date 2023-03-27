8cc6d2be3b10935620e9d4d27354786088583ed8
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-27 15:47:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+
+
+<template>
+  <header>header</header>
+
+  <main>main</main>
+
+  <div class="info-banner">banner1</div>
+
+  <footer>footer</footer>
+
+  <div class="social-banner">banner2</div>
+</template>
+
+
+
+<style></style>
