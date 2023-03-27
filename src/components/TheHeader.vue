c56f0fb44377724fdf3501043557f05ddb21bcba
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-27 17:40:45 +0200

    Header completato

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,52 @@
 <script>
 export default {
-    name: "TheHeader"
+    name: "TheHeader",
+    data() {
+        return {
+            navLinks: [
+                {
+                    href: "#characters",
+                    text: "CHARACTERS",
+                },
+                {
+                    href: "#comics",
+                    text: "COMICS",
+                },
+                {
+                    href: "#movies",
+                    text: "MOVIES",
+                },
+                {
+                    href: "#tv",
+                    text: "TV",
+                },
+                {
+                    href: "#games",
+                    text: "GAMES",
+                },
+                {
+                    href: "#collectibles",
+                    text: "COLLECTIBLES",
+                },
+                {
+                    href: "#videos",
+                    text: "VIDEOS",
+                },
+                {
+                    href: "#fans",
+                    text: "FANS",
+                },
+                {
+                    href: "#news",
+                    text: "NEWS",
+                },
+                {
+                    href: "#shop",
+                    text: "SHOP",
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -8,13 +54,15 @@ export default {
 <template>
     <header>
         <div class="container">
-            <div class="logo">Logo</div>
+            <div class="logo">
+                <img src="../assets/dc-logo.png" alt="Logo Comics">
+            </div>
 
             <ul class="navbar">
-                <li><a class="nav-link" href="#">Testo</a></li>
-                <li><a class="nav-link" href="#">Testo</a></li>
-                <li><a class="nav-link" href="#">Testo</a></li>
-                <li><a class="nav-link" href="#">Testo</a></li>
+                <li v-for="link in navLinks">
+                    <a class="nav-link" :href="link.href">{{ link.text }}</a>
+                    <div class="line"></div>
+                </li>
             </ul>
         </div>
     </header>
@@ -33,26 +81,51 @@ header {
         display: flex;
         align-items: center;
         justify-content: space-between;
-    }
 
-    .navbar {
-        display: flex;
-        height: 100%;
-    }
+        img {
+            height: 80px;
+        }
 
-    li {
-        list-style: none;
-        height: 100%;
-        margin: 0 1rem;
-
-        a {
-            color: black;
-            text-decoration: none;
-            height: 100%;
-            display: block;
+        .navbar {
             display: flex;
-            align-items: center;
+            height: 100%;
+        }
+
+        li {
+            list-style: none;
+            height: 100%;
+            margin: 0 1rem;
+            position: relative;
+
+            a {
+                color: black;
+                text-decoration: none;
+                height: 100%;
+                display: block;
+                display: flex;
+                align-items: center;
+                font-size: 0.8rem;
+                font-weight: 600;
+
+                &:hover {
+                    color: #0c7cec;
+                }
+            }
+
+            .line {
+                position: absolute;
+                width: 100%;
+                height: 7px;
+                background-color: #0c7cec;
+                bottom: 0;
+                display: none;
+            }
+
+            &:hover .line {
+                display: block;
+            }
         }
     }
+
 }
 </style>
